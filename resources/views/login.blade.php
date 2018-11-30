710ec9c34cbb834fcfdec0b7c6e008efb9abf712
SanuMarma <sanumarma1717@gmail.com>
2018-11-30 21:33:51 +0600

    create view and route for login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,93 +1,96 @@
-<html>
-  <head>
-
-  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 
 <style>
-  body#LoginForm{ background-image:url("https://hdwallsource.com/img/2014/9/blur-26347-27038-hd-wallpapers.jpg"); background-repeat:no-repeat; background-position:center; background-size:cover; padding:10px;}
+    body
+{
+    
+    font-family:JF Flat;
+    color:#5687C4;
+}
+body#LoginForm{ background-image:url("http://www.teneriffedoctors.com.au/wp-content/uploads/doctor_banner.jpg"); background-repeat:no-repeat; background-position:center; background-size:cover; padding:10px;}
+body#LoginForm {
+  margin:auto;
+  padding-bottom: 272px;
+}
 
 .form-heading { color:#fff; font-size:23px;}
 .panel h2{ color:#444444; font-size:18px; margin:0 0 8px 0;}
 .panel p { color:#777777; font-size:14px; margin-bottom:30px; line-height:24px;}
-.login-form .form-control {
-  background: #f7f7f7 none repeat scroll 0 0;
-  border: 1px solid #d4d4d4;
-  border-radius: 4px;
-  font-size: 14px;
-  height: 50px;
-  line-height: 50px;
-}
-.main-div {
-  background: #ffffff none repeat scroll 0 0;
-  border-radius: 2px;
-  margin: 10px auto 30px;
-  max-width: 38%;
-  padding: 50px 70px 70px 71px;
-}
 
-.login-form .form-group {
-  margin-bottom:10px;
+.img
+{
+    width:80px;
+    height:75px;
 }
-.login-form{ text-align:center;}
-.forgot a {
-  color: #777777;
-  font-size: 14px;
-  text-decoration: underline;
+.input-group input[type="email"]
+{
+    font-weight:bolder;
+    color:#5687C4;
+    background-color:#DBE3ED;
+    width:420px;
 }
-.login-form  .btn.btn-primary {
-  background: #f0ad4e none repeat scroll 0 0;
-  border-color: #f0ad4e;
-  color: #ffffff;
-  font-size: 14px;
-  width: 100%;
-  height: 50px;
-  line-height: 50px;
-  padding: 0;
+.input-group input[type="password"]
+{
+    font-weight:bolder;
+    color:#5687C4;
+    background-color:#DBE3ED;
+    width:300px;
 }
-.forgot {
-  text-align: left; margin-bottom:30px;
+div >label
+{
+    font-size:14pt;
+    color:#5687C4;
 }
-.botto-text {
-  color: #ffffff;
-  font-size: 14px;
-  margin: auto;
+label#remember
+{
+    font-size:12pt;
+    padding-left:35px;
 }
-.login-form .btn.btn-primary.reset {
-  background: #ff9900 none repeat scroll 0 0;
-}
-.back { text-align: left; margin-top:10px;}
-.back a {color: #444444; font-size: 13px;text-decoration: none;}
-
 </style>
 
-  </head>
 <body id="LoginForm">
-<div class="container">
-<h1 class="form-heading">login Form</h1>
-<div class="login-form">
-    <div class="main-div">
-      <div class="panel">
-          <p>Please enter your email and password</p>
-     </div>
-      <form id="Login" method="post" action="{{ URL::to('loginStore')}}">
-        {{ csrf_field() }}
-
-          <div class="form-group">
-              <input type="email" class="form-control" name="email" placeholder="Email Address">
-          </div>
-          <div class="form-group">
-              <input type="password" class="form-control" name="password" placeholder="Password">
-
-          </div>
-          <button type="submit" class="btn btn-primary">Login</button>
-      </form>
+  <div class="container">
+  <div class="row">
+    
+    <h3> Medical Login Form</h3>
+  </div>
+  <p>Please enter your email and password</p>
+   <form id="Login" method="post" action="{{ URL::to('loginStore')}}">{{ csrf_field() }}
+    <div class="form-group">
+        <div class="row">
+            <h2><img class="img-circle img" src="http://icons.iconarchive.com/icons/iconshock/super-vista-medical/256/stethoscope-icon.png"/>Login our system</h2>
+        </div>
+        <div class="row">
+           <div class="form-group">
+               <label for="inputEmail" class="col-sm-2 control-label">Email:</label>
+                <div class="input-group">
+                   <span class="input-group-btn">
+                        <button class="btn btn-primary glyphicon glyphicon-envelope" type="button"></button>
+                    </span>
+                   <input type="email" class="form-control" name="email" placeholder="Email">
+                </div>  
+           </div>
+        </div>  
+        <div class="row">
+           <div class="form-group">
+               <label for="inputPassword" class="col-sm-2 control-label">Password:</label>
+               <div class="input-group">
+                   <span class="input-group-btn">
+                        <button class="btn btn-primary glyphicon glyphicon-eye-close" type="button"></button>
+                    </span>
+                    <input type="Password" class="form-control" name="password" placeholder="Password">  
+               </div>
+            </div>     
+        </div>
+       
+        <div class="row">
+        <div class="col-sm-6"></div>
+            <button type="submit" class="col-sm-1 btn btn-primary btn-lg active" role="button">Login</button>
+        </div>
+       
     </div>
-</div>
-</div>
-</div>
-
-
-</body>
-</html>
+  </form>
+  </div>
+</body>  
